c5195623670dc9dbb94cdc5bfa6734650158d0a4
Warfork <51063252+Warfork@users.noreply.github.com>
2023-02-09 12:16:39 -0500

    oops sorry

--- source/ref_gl/stb_image.cpp
+++ source/ref_gl/stb_image.cpp
@@ -1,2 +1,2 @@
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include "stb_image_resize.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
--- source/ref_gl/stb_image_write.cpp
+++ source/ref_gl/stb_image_write.cpp
@@ -1,2 +1,2 @@
-#define STB_RECT_PACK_IMPLEMENTATION
-#include "stb_rect_pack.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
