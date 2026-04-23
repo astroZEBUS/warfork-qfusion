b52701b6f40117c5ea40999eb51f16cef2fc3d80
astroZEBUS <iamzebus@gmail.com>
2026-04-23 18:55:58 -0400

    Add Black Option to OutlinePlayers
    Adds another option to cg_outlineplayers, 1 is black, 2 is color.

--- source/cgame/cg_ents.cpp
+++ source/cgame/cg_ents.cpp
@@ -648,12 +648,15 @@ static void CG_AddLinkedModel( centity_t *cent )
 }
 
 /*
-* CG_AddCentityOutLineEffect
-*/
+ * CG_AddCentityOutLineEffect
+ */
 void CG_AddCentityOutLineEffect( centity_t *cent )
 {
-	CG_AddColoredOutLineEffect( &cent->ent, cent->effects, 
-		cent->outlineColor[0], cent->outlineColor[1], cent->outlineColor[2], cent->outlineColor[3] );
+	if( cg_outlinePlayers->integer >= 2 ) {
+		CG_AddColoredOutLineEffect( &cent->ent, cent->effects, cent->outlineColor[0], cent->outlineColor[1], cent->outlineColor[2], cent->outlineColor[3] );
+	} else if( cg_outlinePlayers->integer == 1 ) {
+		CG_AddColoredOutLineEffect( &cent->ent, cent->effects, 0, 0, 0, cent->outlineColor[3] );
+	}
 }
 
 //==========================================================================
--- source/cgame/cg_main.cpp
+++ source/cgame/cg_main.cpp
@@ -784,7 +784,7 @@ static void CG_RegisterVariables( void )
 	cg_gibs =		trap_Cvar_Get( "cg_gibs", "1", CVAR_ARCHIVE );
 	cg_outlineModels =	trap_Cvar_Get( "cg_outlineModels", "1", CVAR_ARCHIVE );
 	cg_outlineWorld =	trap_Cvar_Get( "cg_outlineWorld", "0", CVAR_ARCHIVE );
-	cg_outlinePlayers =	trap_Cvar_Get( "cg_outlinePlayers", "1", CVAR_ARCHIVE );
+	cg_outlinePlayers =	trap_Cvar_Get( "cg_outlinePlayers", "2", CVAR_ARCHIVE );
 	cg_drawEntityBoxes =	trap_Cvar_Get( "cg_drawEntityBoxes", "0", CVAR_DEVELOPER );
 	cg_showObituaries =	trap_Cvar_Get( "cg_showObituaries", va( "%i", CG_OBITUARY_HUD|CG_OBITUARY_CENTER ), CVAR_ARCHIVE );
     cg_damageNumbers = trap_Cvar_Get( "cg_damageNumbers", "1", CVAR_ARCHIVE );
