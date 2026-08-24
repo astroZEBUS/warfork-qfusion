astroZEBUS/warfork-qfusion

commit 014d1109eb325d1d1efe6719c8a02bd0e7591528

Feat: Bots collide with monsterclip brushes.
- Add PMFEAT_BOTCLIP to make bot movement collide with CONTENTS_MONSTERCLIP brushes, same as playerclip. The PMFEAT bit is transmitted in snapshots but has no client-side effect since bots are server-simulated only.
- Also stop bots from bunnyhopping in place.

source/gameshared/gs_pmove.c
@@ -1847,6 +1847,8 @@ void Pmove( pmove_t *pmove )
 			pm->playerState->pmove.pm_flags &= ~PMF_NO_PREDICTION;
 		if( pm->playerState->pmove.stats[PM_STAT_FEATURES] & PMFEAT_GHOSTMOVE )
 			pm->contentmask = MASK_DEADSOLID;
+		else if( pm->playerState->pmove.stats[PM_STAT_FEATURES] & PMFEAT_BOTCLIP )
+			pm->contentmask = MASK_MONSTERSOLID;
 		else
 			pm->contentmask = MASK_PLAYERSOLID;
 		break;

source/gameshared/gs_public.h
@@ -672,6 +672,7 @@ float GS_FrameForTime( int *frame, unsigned int curTime, unsigned int startTimeS
 #define	PMFEAT_ITEMPICK			( 1<<10 )
 #define	PMFEAT_GUNBLADEAUTOATTACK ( 1<<11 )
 #define	PMFEAT_WEAPONSWITCH		( 1<<12 )
+#define	PMFEAT_BOTCLIP			( 1<<13 )
 
 #define PMFEAT_ALL				( 0xFFFF )
 #define PMFEAT_DEFAULT			( PMFEAT_ALL & ~PMFEAT_GHOSTMOVE )

source/game/ai/ai_class_dmbot.cpp
@@ -280,7 +280,23 @@ void BOT_DMclass_SpecialMove( edict_t *self, vec3_t lookdir, vec3_t pathdir, use
 	if( bunnyhop )
 	{
 		if( self->groundentity )
-			ucmd->upmove = 1;
+		{
+			vec3_t hvel, hlook;
+			float hspeed;
+
+			VectorCopy( self->velocity, hvel );
+			hvel[2] = 0;
+			hspeed = VectorLengthFast( hvel );
+			if( hspeed >= self->r.client->ps.pmove.stats[PM_STAT_MAXSPEED] )
+			{
+				VectorScale( hvel, 1.0f / hspeed, hvel );
+				VectorCopy( lookdir, hlook );
+				hlook[2] = 0;
+				VectorNormalize( hlook );
+				if( DotProduct( hvel, hlook ) >= 0.707f ) // cos(45°)
+					ucmd->upmove = 1;
+			}
+		}
 
 #if 0
 		// fake strafe-jumping acceleration

source/game/p_client.cpp
@@ -612,6 +612,8 @@ void G_ClientRespawn( edict_t *self, bool ghost )
 		client->ps.pmove.stats[PM_STAT_FEATURES] = static_cast<unsigned short>(PMFEAT_DEFAULT);
 		if( !g_allow_bunny->integer )
 			client->ps.pmove.stats[PM_STAT_FEATURES] &= ~( PMFEAT_AIRCONTROL|PMFEAT_FWDBUNNY );
+		if( AI_GetType( self->ai ) == AI_ISBOT )
+			client->ps.pmove.stats[PM_STAT_FEATURES] |= PMFEAT_BOTCLIP;
 	}
 
 	ClientUserinfoChanged( self, client->userinfo );

commit 79cd7f508f5784cb2484811c78002475aba70bdb

Fix: Bug introduced in 014d1109.
Fix playerclip being disabled by having PMFEAT_BOTCLIP listed in the PMFEAT_DEFAULT mask which is applied to all players, bots or otherwise.

source/gameshared/gs_pmove.c
@@ -1847,10 +1847,10 @@ void Pmove( pmove_t *pmove )
 			pm->playerState->pmove.pm_flags &= ~PMF_NO_PREDICTION;
 		if( pm->playerState->pmove.stats[PM_STAT_FEATURES] & PMFEAT_GHOSTMOVE )
 			pm->contentmask = MASK_DEADSOLID;
-		else if( pm->playerState->pmove.stats[PM_STAT_FEATURES] & PMFEAT_BOTCLIP )
-			pm->contentmask = MASK_MONSTERSOLID;
 		else
 			pm->contentmask = MASK_PLAYERSOLID;
+		if( pm->playerState->pmove.stats[PM_STAT_FEATURES] & PMFEAT_BOTCLIP )
+			pm->contentmask |= CONTENTS_MONSTERCLIP;
 		break;
 	}
 

source/gameshared/gs_public.h
@@ -675,7 +675,7 @@ float GS_FrameForTime( int *frame, unsigned int curTime, unsigned int startTimeS
 #define	PMFEAT_BOTCLIP			( 1<<13 )
 
 #define PMFEAT_ALL				( 0xFFFF )
-#define PMFEAT_DEFAULT			( PMFEAT_ALL & ~PMFEAT_GHOSTMOVE )
+#define PMFEAT_DEFAULT			( PMFEAT_ALL & ~( PMFEAT_GHOSTMOVE|PMFEAT_BOTCLIP ) )
 
 enum
 {